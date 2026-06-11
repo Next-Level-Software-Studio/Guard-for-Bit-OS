b4edba5189d68dea1089c6f2e4354b9e7c6c6a07
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-11 21:44:33 +0100

    .

--- overlay/usr/share/bit-os/guard/firewall.py
+++ overlay/usr/share/bit-os/guard/firewall.py
@@ -1,8 +1,8 @@
 import json
 from pathlib import Path
-with open("/usr/share/bit-os/guard/non-official-ports-services.json", "r") as f:
+with open("/usr/share/bit-os/guard/well-known-ports.json", "r") as f:
     non_official = json.load(f)
-
+official = 
 def on():
     pass
 def off():
