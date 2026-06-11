f91f3f3a8cce9307d847fd4560d5713f97ce69a9
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-11 21:55:34 +0100

    .

--- overlay/usr/share/bit-os/guard/firewall.py
+++ overlay/usr/share/bit-os/guard/firewall.py
@@ -1,8 +1,19 @@
 import json
 from pathlib import Path
+
 with open("/usr/share/bit-os/guard/well-known-ports.json", "r") as f:
     non_official = json.load(f)
-official = 
+
+official = None
+
+def _getservice(port = int):
+    if port <= 1023:
+        return "well_known"
+    elif port >= 1024 and port <= 49151:
+        return "registered"
+    elif port >= 49152 and port <= 65535:
+        return "private"
+
 def on():
     pass
 def off():
