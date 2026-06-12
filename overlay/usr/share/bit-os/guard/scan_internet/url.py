8f8920937acd5f37005f3583393551e8bb0fc8e2
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-12 17:07:48 +0100

    .

--- overlay/usr/share/bit-os/guard/scan_internet/url.py
+++ overlay/usr/share/bit-os/guard/scan_internet/url.py
@@ -1,2 +1,9 @@
-def url(data = str):
-    if data
+def url(data = str, level = int):
+    if (level == 0) or (level > 9):
+        print("invalid level")
+
+    dangerlevel = 0
+
+    if level == 1:
+        if data.startswith("http://"):
+            dangerlevel = 100
