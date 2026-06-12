51ed94dfbd38f848dbd12c63620860d7509e079e
Lumarysz <230233445+Lumarysz@users.noreply.github.com>
2026-06-12 22:26:19 +0100

    Adiciona suporte para o nível de perigo e verifica a existência da URL

--- overlay/usr/share/bit-os/guard/scan_internet/url.py
+++ overlay/usr/share/bit-os/guard/scan_internet/url.py
@@ -1,7 +1,20 @@
+import requests, sys
 def url(data = str, level = int):
-    if (level == 0) or (level > 9):
-        print("invalid level")
+    # verificar se a URL existe
+    answer = requests.head(data, allow_redirects=True, timeout=5)
+    if answer.status_code == 200:
+        exists = True
+    elif answer.status_code == 404:
+        exists = False
+    else:
+        print(f"{data} doesn't exists.")
 
+    #verificação de level incorreto
+
+    if (level == 0) or (level > 9):
+        sys.exit(1)
+    elif not exists:
+        sys.exit(1)
     dangerlevel = 0
 
     if level == 1:
